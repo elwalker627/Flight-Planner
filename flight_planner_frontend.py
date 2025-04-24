c2a90522ec150cfa844fc2df30a7630e843b5971
elwalker627 <elwalker627@gmail.com>
2025-04-23 20:46:22 -0600

    Trying to fix arcs

--- flight_planner_frontend.py
+++ flight_planner_frontend.py
@@ -18,6 +18,7 @@ def update_plane_schedule(plane_id, date):
         returner = cursor.fetchall()
         cursor.close()
         connection.close()
+        print("Returner:", returner)
         return returner
     else:
         return None
@@ -35,9 +36,11 @@ st.title("Flight Schedule Viewer")
 
 plane_id = st.text_input("Enter Plane ID:", "1")
 if st.button("Update Schedule"):
+    print("Buttone clicked")
     date = datetime.now()
     new_data = update_plane_schedule(plane_id, date)
     if new_data != None:
+        print("New data")
         flight_data = new_data
 
 # Filter your real dataset here
